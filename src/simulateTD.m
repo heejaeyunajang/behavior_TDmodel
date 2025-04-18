cc5da5d87376e81eb842062fa538b4382884fd63
heejaeyunajang <30902301+heejaeyunajang@users.noreply.github.com>
2025-04-18 12:11:16 -0400

    return RPE

--- src/simulateTD.m
+++ src/simulateTD.m
@@ -1,8 +1,8 @@
-function [modelITI, modelValue, modelITI_] = simulateTD(ratTrial)
+function [modelITI, modelITI_, modelValue, RPE] = simulateTD(ratTrial)
 
 % Simulate trial initiation time with a vanilla TD model
 alpha = 0.6; % learning rate
-[modelITI_, ~, modelValue_, ~] = generateITI_vanillaTD(alpha, ratTrial);
+[modelITI_, RPE, modelValue_, ~] = generateITI_vanillaTD(alpha, ratTrial);
 
 % Exclude violation trials
 modelValue = modelValue_(ratTrial.vios~=1);
