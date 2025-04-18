7401a62e01f231d4841efd6092ed2982c70f6d2a
heejaeyunajang <30902301+heejaeyunajang@users.noreply.github.com>
2025-04-18 19:40:00 -0400

    move utils scripts to utils_core repo

--- utils/blsubtract.m
+++
@@ -1,8 +0,0 @@
-function data_bc = blsubtract(T, t1bl, t2bl, data)
-    
-    [~ , t1] = min(abs(T+t1bl));
-    [~ , t2] = min(abs(T+t2bl));
-    
-    baseline = min(mean(data(:,t1:t2), 'omitnan'));
-    data_bc = data - baseline;
-end
